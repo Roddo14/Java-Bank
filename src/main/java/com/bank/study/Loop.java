30fb8e5cd51c6ec2adb1ff9f3e170fb7cace7101
Rodrigo Pupo Ribeiro <rofla@yahoo.com>
2025-12-27 14:21:28 +0000

    loop

---
+++ src/main/java/com/bank/study/Loop.java
@@ -0,0 +1,16 @@
+package com.bank.study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loop {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get (i));
+        }
+    }
+}
